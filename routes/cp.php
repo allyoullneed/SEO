c07a368cce0174ae40bdd3a21bae7bde2fe6091c
FX David <fxavierdavid@gmail.com>
2026-02-08 22:39:20 +0100

    Update

--- routes/cp.php
+++ routes/cp.php
@@ -1,7 +1,7 @@
 <?php
 use Illuminate\Support\Facades\Route;
 
-Route::group(['middleware' => ['statamic.cp.authenticated'], 'namespace' => 'AltDesign\AltSeo\Http\Controllers'], function() {
+Route::group(['middleware' => ['statamic.cp.authenticated'], 'namespace' => 'AllYouNeed\Seo\Http\Controllers'], function() {
     // Settings
     Route::get('/alt-design/alt-seo/', 'AltController@index')->name('alt-seo.index');
     Route::post('/alt-design/alt-seo/', 'AltController@update')->name('alt-seo.update');
